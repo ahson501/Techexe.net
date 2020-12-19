e9114ea55112571888308aaaddf5f8ecd9129750
ahson-spec <techexe.net@gmail.com>
2020-12-19 07:57:51 +0500

    commit 19-12-2020

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,18 @@
+import React, {usestate} from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <nav classname="navbar">
+            <div classname="navbar-container">
+            </div>
+        </nav>
+        </>
+        
+            
+        
+    )
+}
+
+export default Navbar
 

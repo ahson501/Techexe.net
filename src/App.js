e1d0f8761975d58fbc5fef2c6ab46ab08c72ca47
ahson-spec <techexe.net@gmail.com>
2020-12-19 22:00:29 +0500

    19-12-2020

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Navbar from '.components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>react tech1</h1>
-    </div>
-  );
+  return (<>
+    <Router>
+      <Navbar/>
+    <switch>
+      <Route path="/" exact />
+      </switch>
+      </Router>
+  </>);
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, {usestate} from 'react'
-
+import { link } from 'react-router-dom';
 function Navbar() {
     return (
         <>
         <nav classname="navbar">
             <div classname="navbar-container">
-                <link to="/" classname="navbar-logo"> Tech </link>
+                <link to="/" classname="navbar-logo"> Tech i.fab.fa-typo3   
+                </link>
+                
             </div>
         </nav>
         </>

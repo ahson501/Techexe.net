60d9950a3864a62d3f63c81811eb2e3773f614d3
ahson501 <74943432+ahson501@users.noreply.github.com>
2020-12-25 23:11:58 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
-import React, {usestate} from 'react'
+import React, {usestate} from 'react';
 import { Link } from 'react-router-dom';
 function Navbar() {
+    const [click, setClick] = usestate(false);
     return (
         <>
         <nav classname="navbar">
             <div classname="navbar-container">
                 <link to="/" classname="navbar-logo"> 
-                Tech <i classname="fab fa-typo3"></i>   
-                </link>
+                Tech <i classname="fab fa-typo3"></i> 
+           </link>
+            <div classname="menu-item">
+                <i classname={click ?" 'fas fa-time' : 'fas fa-bars'}/> 
+                </div>
               </div>
            </nav>
         </>

0c5be1e52e0f4b5ef1b234a2854c4429f90f5ea4
ahson501 <74943432+ahson501@users.noreply.github.com>
2020-12-26 02:20:24 +0500

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,18 @@
-
+import React from 'react';
+import './Button.CSS';
+import { Link } from 'react-router-dom';
+const STYLES = ['btn--primary' , 'btn--outline'];
+const SIZES = ['btn-medium' ,'btn-large'];
+export const button =({children, type, onClick, buttonStyle, buttonSize}) => {const checkButtonStyle=Styles.includes(buttonstyle) ? buttonStyle : STYLES[0];
+const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : STYLE[0]; 
+return(<link to = '/home4' classname='btn-mobile'> 
+         <button
+classname = {'btn ${checkButtonStyle} ${checkButtonSize}'}
+onClick={onClick}>
+type={type}
+       >
+         {childeren}
+         </button>
+         </link>
+        ) 
+        };                                                                          }

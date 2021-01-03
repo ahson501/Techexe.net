75eb2ee76682ac8dfc4f268c0ef0aa39563a1b4e
ahson-spec <techexe.net@gmail.com>
2021-01-03 23:02:10 +0500

    --

--- src/App.js
+++ src/App.js
@@ -1,19 +1,1 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
 
-function App() {
-  return (
-    <>
-    <Router>
-      <Navbar />
-    <switch>
-      <Route path="/" exact />
-      </switch>
-      </Router>
-  </>
-  );
-}
-
-export default App;

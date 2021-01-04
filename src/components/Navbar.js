896b0e34322a1a29712c2513e50c46f10f688645
ahson-spec <techexe.net@gmail.com>
2021-01-04 07:58:07 +0500

    4-1-2027581

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, {usestate} from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {return (
+function Navbar() {
+    return (
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
-        <link to='/' className='navbar-logo'> 
+          <link to='/' className='navbar-logo'> 
                 Tech <i className='fab fa-typo3'/> 
            </link>
         </div>

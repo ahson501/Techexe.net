a0c1e34b273474186841ba3b8927a80e36ccb568
ahson-spec <techexe.net@gmail.com>
2021-01-24 07:32:04 +0500

    21-1-2021

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, {usestate} from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router';
 
 function Navbar() {
     return (
